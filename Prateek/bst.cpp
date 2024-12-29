a6c7e0c9988c82cf338beb6b6d44cd3f0364a788
PrateekAriga <01fe23bcs203@kletech.ac.in>
2024-12-30 00:46:43 +0530

    Create bst.cpp

---
+++ Prateek/bst.cpp
@@ -0,0 +1,6 @@
+Node* insert(Node* root, int key) {
+    if (!root) return new Node(key);
+    if (key < root->data) root->left = insert(root->left, key);
+    else root->right = insert(root->right, key);
+    return root;
+}
