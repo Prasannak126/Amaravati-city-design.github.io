29e1cc98716d0d7676d7cade8cfb8fe0dd25cf2b
Prasannak126 <01fe23bcs126@kletech.ac.in>
2024-12-29 15:49:57 +0530

    Update Dijkstra's.cpp

--- Prasanna/Codes/Dijkstra's.cpp
+++ Prasanna/Codes/Dijkstra's.cpp
@@ -2,15 +2,15 @@
 #include <vector>
 using namespace std;
 
-const int INF = 100000;
+const int INF = 100000; 
 
 struct Edge {
-    int src, dest, distance, sensitivity, weight;
+    int src, dest, weight;
 };
 
 class Graph {
-    int V;
-    vector<pair<int, int>>* adjList;
+    int V; // Number of vertices
+    vector<pair<int, int>>* adjList; 
 public:
     Graph(int V) {
         this->V = V;
@@ -21,8 +21,9 @@ public:
         adjList[u].push_back({v, weight});
     }
 
-    vector<int> dijkstra(int src) {
-        vector<int> dist(V, INF);
+    void dijkstra(int src) {
+        vector<int> dist(V, INF); 
+        vector<int> parent(V, -1);
         vector<bool> visited(V, false);
 
         dist[src] = 0;
@@ -35,52 +36,44 @@ public:
                 }
             }
 
-            if (dist[u] == INF)
-                break;
+            if (dist[u] == INF) break;
 
             visited[u] = true;
-            for (auto neighbor : adjList[u]) {
+
+            for (auto& neighbor : adjList[u]) {
                 int v = neighbor.first;
                 int weight = neighbor.second;
-
                 if (dist[u] + weight < dist[v]) {
                     dist[v] = dist[u] + weight;
+                    parent[v] = u;
                 }
             }
         }
-
-        return dist;
+        cout << "Vertex   Distance from Source   Parent" << endl;
+        for (int i = 0; i < V; ++i) {
+            cout << i << " \t\t " << dist[i] << " \t\t " << parent[i] << endl;
+        }
     }
 };
 
 int main() {
-    vector<Edge> edges = {
-        {0, 1, 100, 1, 101}, // EA
-        {0, 2, 150, 2, 152}, // EB
-        {0, 3, 80,  1, 81},  // EC
-        {0, 4, 200, 3, 203}, // ED
-        {1, 2, 50,  1, 51},  // AB
-        {1, 3, 120, 2, 122}, // AC
-        {1, 4, 180, 3, 183}, // AD
-        {2, 3, 70,  1, 71},  // BC
-        {2, 4, 100, 2, 102}, // BD
-        {2, 5, 150, 1, 151}, // BE
-        {3, 6, 80,  1, 81},  // CF
-        {4, 6, 120, 2, 122}  // DF
-    };
-
-    int V = 7; // Number of nodes: 0 (Entrance), 1 (A), 2 (B), 3 (C), 4 (D), 5 (E), 6 (F)
+    int V = 6; 
     Graph g(V);
+
+    vector<Edge> edges = {
+        {0, 1, 201}, // A-B
+        {0, 2, 302}, // A-C
+        {1, 3, 153}, // B-D
+        {1, 4, 252}, // B-E
+        {2, 3, 101}, // C-D
+        {2, 5, 402}, // C-F
+        {3, 5, 301}  // D-F
+    };
 
     for (const Edge& edge : edges) {
         g.addEdge(edge.src, edge.dest, edge.weight);
     }
-    vector<int> distances = g.dijkstra(0);
-    char nodes[] = {'E', 'A', 'B', 'C', 'D', 'F'};
-    cout << "Shortest paths from Entrance (Dijkstra's Algorithm):" << endl;
-    for (int i = 0; i < 6; ++i) {
-        cout << "Distance to " << nodes[i] << ": " << distances[i] << endl;
-    }
+    g.dijkstra(0);
 
     return 0;
 }
